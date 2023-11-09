b830d752e7830291932b235407436d8d2e304809
mochi3 <35mochi@gmail.com>
2023-11-10 05:41:13 +0900

    feat: add mock

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+      <>
+        <div class="flex items-center justify-end px-6 py-2 bg-[#fff7e0]">
+            <button class="px-4 py-2 border-solid rounded-md font-bold text-[#302921]">login</button>
+        </div>
+      </>
+    );
+  }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,29 @@
 import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
 
+import { Header } from "../components/Header.tsx";
+
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
+    <>
+      <Header />
+      <div class="px-4 py-8 mx-auto">
+        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+          <img
+            class="my-6"
+            src="/logo.svg"
+            width="128"
+            height="128"
+            alt="the Fresh logo: a sliced lemon dripping with juice"
+          />
+          <h1 class="text-4xl font-bold mb-5">Try Search</h1>
+          <form class="h-16 rounded-md bg-[#e3e2de]">
+            <input type="text" placeholder="search food" class="bg-transparent w-60 px-3 py-2 leading-loose h-full"></input>
+            <button type="submit" class="bg-[#ffcb57] p-4 h-full rounded-md">search</button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
