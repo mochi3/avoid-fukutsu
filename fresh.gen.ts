3cd9fcf001a1e2e33ac6cabd07cc6ecc0837e5f6
mochi3 <35mochi@gmail.com>
2023-11-10 03:04:43 +0900

    rename

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,7 +7,7 @@ import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/api/joke.ts";
 import * as $3 from "./routes/greet/[name].tsx";
 import * as $4 from "./routes/index.tsx";
-import * as $$0 from "./islands/CountDown.tsx";
+import * as $$0 from "./islands/Countdown.tsx";
 import * as $$1 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -19,7 +19,7 @@ const manifest = {
     "./routes/index.tsx": $4,
   },
   islands: {
-    "./islands/CountDown.tsx": $$0,
+    "./islands/Countdown.tsx": $$0,
     "./islands/Counter.tsx": $$1,
   },
   baseUrl: import.meta.url,
